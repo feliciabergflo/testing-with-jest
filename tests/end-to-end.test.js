d88bbfbded914299d8abacac0d9080b879b18ea0
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2022-05-07 19:33:07 +0200

    Nytt försök: Enhet- och end-to-end-test med felaktig kod

--- tests/end-to-end.test.js
+++ tests/end-to-end.test.js
@@ -1,6 +1,4 @@
 const { Builder, By, until } = require("selenium-webdriver");
-const { get } = require("selenium-webdriver/http");
-const { DriverService } = require("selenium-webdriver/remote");
 require("geckodriver");
 
 const fileUnderTest = "file://" + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
@@ -9,6 +7,7 @@ let driver;
 
 // Kontrollerar att Firefox är igång
 beforeAll(async () => {
+console.log(fileUnderTest);
     driver = await new Builder().forBrowser("firefox").build();
     await driver.get(fileUnderTest);
 });
@@ -19,10 +18,9 @@ afterAll(async() => {
 }, defaultTimeout);
 
 
-/* Testar ifall "Pusha till stacken" öppnar en prompt box
-samt visar det senaste tillagda elementet på sidan */
-describe('Clicking "Pusha till stacken"', () => {
-	it('should open a prompt box and display new element in stack', async () => {
+/* Testar ifall "Pusha till stacken" visar det senaste tillagda elementet på sidan */
+describe("Clicking 'Pusha till stacken'", () => {
+	it("should display the new element in stack", async () => {
 		let push = await driver.findElement(By.id('push'));
 		await push.click();
 
@@ -35,19 +33,12 @@ describe('Clicking "Pusha till stacken"', () => {
 	});
 });
 
-/* Testar ifall "Poppa stacken!" öppnar en prompt box
-samt raderar det senaste tillagda elementet på sidan */
+/* Testar ifall "Poppa stacken!" öppnar en prompt box */
 describe("Clicking 'Poppa stacken!'", () => {
     it("should open a prompt box", async () => {
-		let push = await driver.findElement(By.id('push'));
-		await push.click();
-		let pushAlert = await driver.switchTo().alert();
-		await pushAlert.sendKeys("test");   
-        await pushAlert.accept();     
-    
         let pop = await driver.findElement(By.id("pop"));
         await pop.click();
-        let popAlert = await driver.switchTo().alert();
-        await popAlert.accept();
+        let alert = await driver.switchTo().alert();
+        await alert.accept();
     });
 });
